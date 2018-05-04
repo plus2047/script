6cfe3164cf3543d036d5d2b843d7c9ddb59a54d0
plus2047 <xjh_cn@yeah.net>
2018-05-04 09:13:08 +0800

    install scripts

---
+++ install.sh
@@ -0,0 +1,9 @@
+SD=$(dirname "$0")
+PWD=`pwd`
+
+# zsh & oh-my-zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
+~
