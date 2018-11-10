f3ad357d7a76b8ae90c0e08dde79971e57f9f8b6
Xu Jiahao <xujiahao@giska.com>
2018-11-10 13:43:08 +0800

    improve vimrc

--- cmd/maxim.py
+++ cmd/maxim.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 
 maxim_list = [
-        "Catch the MOMENT",
+        "Catch the moment",
         "Cool down & All is well ~",
-        "Think before coding",
+        "Thinking before coding",
         "JUST DO IT!",
         "Forget all the reasons why it won't work and believe the one reason why it will.", 
         "It is not uncommon for people to spend their whole life waiting to start living.",
@@ -11,9 +11,10 @@ maxim_list = [
         "but it's what you do at that moment that determines who you are.",
         "Forget the mistakes of the past and press on to "
         "the greater achievements of the future.",
+        "I am strong because I've been weak. I am fearless because I'vebeen afraid. "
+        "I am wise because I've been foolish.",
         ]
 
 import random
-
 print("\n" + random.choice(maxim_list))
 
