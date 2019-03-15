08cd12be4a135cf82be7b3dea6b52a5c8f805ba6
plus2047 <xjh_cn@yeah.net>
2019-03-15 10:17:19 +0800

    190315 MACC02S18ANG8WN

--- _install.sh
+++ _install.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 
+alias BREW='ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+alias ZSH='sh -c $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)'
+
 echo "need sodo permission for apt."
 sudo echo "sudo ready"
 
-# bin needed: brew (mac), coreutils(mac), python3, zsh, git
+# ===== basic =====
 if [ `uname` = "Darwin" ]; then
     if [ ! -x `command -v brew` ]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        hash -r
+        BREW ; hash -r
     fi
     brew install coreutils git zsh python3
 else
     sudo apt install zsh python3 git
 fi
 hash -r
-# =====
 
-# ===== oh-my-zsh =====
-if [ ! -d "$HOME/.oh-my-zsh" ] 
-then
-    sh -c $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+# ===== apps =====
+if [ `uname` = "Darwin" ]; then
+    brew cask install dozer
 fi
 
-# ===== install config files. =====
-_SD=`dirname "$0"`; SD=`realpath $_SD`; PWD=`pwd`
+# ===== oh-my-zsh =====
+[ ! -d "$HOME/.oh-my-zsh" ] && ZSH
 
+# ===== install config files. =====
+# vim & screen
+_SD=`dirname "$0"`; SD=`realpath $_SD`; PWD=`pwd`
 _TS=`date +"_%y%m%d"`
 if [ ! -e $HOME/.vimrc$_TS ]; then
     mv $HOME/.screenrc $HOME/.screenrc$_TS &> /dev/null
@@ -34,13 +37,12 @@ rm $HOME/.screenrc &> /dev/null
 rm $HOME/.vimrc &> /dev/null
 ln -Fs $SD/config/screenrc $HOME/.screenrc
 ln -Fs $SD/config/vimrc $HOME/.vimrc
-
-# zshrc
+# zshrc tail
 SHCMD="source $SD/config/anyshrc"
 TAIL=`tail -1 $HOME/.zshrc`
 [ ! "$TAIL" == "$SHCMD" ] && echo $SHCMD >> $HOME/.zshrc
 
-# # install ssh public key & config but not install private key.
+# # ===== ssh public key & config =====
 # mkdir $HOME/.ssh &> /dev/null
 # chmod 700 $HOME/.ssh
 # _C=$HOME/.ssh/config
