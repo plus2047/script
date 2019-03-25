207ac94b4d3381d921e6e8d278a6fce02f4fb9ad
plus2047 <xjh_cn@yeah.net>
2019-03-25 18:53:28 +0800

    190325

--- _install.sh
+++ _install.sh
@@ -3,57 +3,71 @@
 echo "need sodo permission for apt."
 sudo echo "sudo ready"
 
-# ===== basic =====
-if [ `uname` = "Darwin" ]; then
-    if [ ! -x `command -v brew` ]; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        hash -r
-    fi
+# ===== installation functions =====
+function install_mac_app {
+    [ ! -x `command -v brew` ] && return
+    echo "install mac app..."
     brew install coreutils git zsh python3
-else
-    sudo apt install zsh python3 git
-fi
-hash -r
-
-# ===== apps =====
-if [ `uname` = "Darwin" ]; then
     brew cask install dozer
     brew cask install karabiner-elements
     brew cask install hammerspoon
-fi
+}
 
-# ===== oh-my-zsh =====
-[ ! -d "$HOME/.oh-my-zsh" ] && sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+function install_linux_app {
+    [ ! `uname` = "Linux" ] && return
+    [ ! -x `command -v apt` ] && return
+    echo "install linux app..."
+    sudo apt install zsh python3 git
+}
 
-# ===== install config files. =====
-# vim & screen
-_SD=`dirname "$0"`; SD=`realpath $_SD`; PWD=`pwd`
-_TS=`date +"_%y%m%d"`
-if [ ! -e $HOME/.vimrc$_TS ]; then
-    mv $HOME/.screenrc $HOME/.screenrc$_TS &> /dev/null
-    mv $HOME/.vimrc $HOME/.vimrc$_TS &> /dev/null
-fi
-rm $HOME/.screenrc &> /dev/null
-rm $HOME/.vimrc &> /dev/null
-ln -Fs $SD/config/screenrc $HOME/.screenrc
-ln -Fs $SD/config/vimrc $HOME/.vimrc
-# zshrc tail
-SHCMD="source $SD/config/anyshrc"
-TAIL=`tail -1 $HOME/.zshrc`
-[ ! "$TAIL" == "$SHCMD" ] && echo $SHCMD >> $HOME/.zshrc
+function install_brew {
+    [ ! `uname` = "Darwin" ] && return
+    [ -x `command -v brew` ] && return
+    echo "install homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    hash -r
+}
 
-# # ===== ssh public key & config =====
-# mkdir $HOME/.ssh &> /dev/null
-# chmod 700 $HOME/.ssh
-# _C=$HOME/.ssh/config
-# _A=$HOME/.ssh/authorized_keys
-# _TS=`date +"_%y%m%d"`
-# [ -e $_C -a ! -e $_C$_TS ] && mv $_C $_C$_TS
-# [ -e $_A -a ! -e $_A$_TS ] && mv $_A $_A$_TS
-# cp $SD/ssh/id_rsa.pub $_A
-# cp $SD/ssh/config $_C
-# chmod 644 $_C $_A
+function install_myzsh {
+    [ -d "$HOME/.oh-my-zsh" ] && return
+    echo "install oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
 
-# return to origin dir.
-cd $PWD
+function backup_and_rm {
+    _TS=`date +"_%y%m%d"`
+    [ ! -e $1$_TS ] && mv $1 $1$_TS &> /dev/null
+    rm $1 &> /dev/null
+}
+
+function link_to {
+    backup_and_rm $2
+    ln -Fs $1 $2
+}
+
+function copy_to {
+    backup_and_rm $2
+    cp $1 $2
+}
+
+function append_to_file {
+    touch $2
+    TAIL=`tail -1 $2`
+    [ ! "$TAIL" == "$1" ] && echo $1 >> $2
+}
+
+# ===== install all =====
+install_brew
+install_mac_app
+install_linux_app
+install_myzsh
+
+script_dir=$(dirname $0)
+script_dir=$(realpath $script_dir)
+
+link_to $script_dir/config/screenrc $HOME/.screenrc
+link_to $script_dir/config/vimrc $HOME/.vimrc
+
+append_to_file "source $script_dir/config/anyshrc" $HOME/.zshrc
+append_to_file "source $script_dir/config/anyshrc" $HOME/.bashrc
 
