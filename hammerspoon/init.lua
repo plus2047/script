13c79e335dcfeb4eb866c0fe9e8c88948d27c5d3
jiahao.xu <jiahao.xu@bytedance.com>
2023-06-05 10:18:10 +0800

    app switcher

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,24 +1,18 @@
 hyper = {"alt"}
-
--- basic remap
--- vim arrow + hyper escape
-hs.hotkey.bind(hyper, ',', nil, function()
-  hs.reload()
-end)
-
+hs.hotkey.bind(hyper, ',', nil, function() hs.reload() end)
 require("remap")
 
 -- window management
--- position & shape
 local hk = require("hs.hotkey")
 local wm = require("window-management")
 
+hk.bind(hyper, 'q', wm.leftHalf)
+hk.bind(hyper, 'w', wm.bottomHalf)
+hk.bind(hyper, 'e', wm.topHalf)
+hk.bind(hyper, 'r', wm.rightHalf)
+
 hk.bind(hyper, 'f', wm.maximizeWindow)
 hk.bind(hyper, 'a', wm.centerOnScreen)
-hk.bind(hyper, 'q', wm.leftHalf)
-hk.bind(hyper, 'r', wm.rightHalf)
-hk.bind(hyper, 'e', wm.topHalf)
-hk.bind(hyper, 'w', wm.bottomHalf)
 hk.bind(hyper, 'b', wm.ToNextScreen)
 
 -- move mouse between window
@@ -27,15 +21,13 @@ hk.bind(hyper, 'v', function()
     local nextScreen = screen:next()
     local rect = nextScreen:fullFrame()
     local center = hs.geometry.rectMidPoint(rect)
- 
     hs.mouse.absolutePosition(center)
 end)
 
+-- windows switcher
 windowFilter = hs.window.filter.new()
 windowFilter:setCurrentSpace(true)
 switcher = hs.window.switcher.new(windowFilter)
--- hs.window.switcher.ui.showThumbnails = false
--- hs.window.switcher.ui.showTitles = false
 hs.window.switcher.ui.showSelectedThumbnail = false
 hs.window.switcher.ui.showSelectedTitle = false
 hk.bind("alt",'tab', function()switcher:next()end)
@@ -43,10 +35,15 @@ hk.bind('alt-shift','tab',function()switcher:previous()end)
 
 -- clipboard tool
 hs.loadSpoon("ClipboardTool")
-spoon.ClipboardTool:bindHotkeys({
-  toggle_clipboard = {hyper, "c"}
-})
+spoon.ClipboardTool:bindHotkeys({toggle_clipboard = {hyper, "c"}})
 spoon.ClipboardTool.show_in_menubar = false
 spoon.ClipboardTool:start()
+
+-- app shortcut
+hs.hotkey.bind(hyper, "d", function() hs.application.launchOrFocus("Dictionary") end)
+hs.hotkey.bind(hyper, "x", function() hs.application.launchOrFocus("Visual Studio Code") end)
+hs.hotkey.bind(hyper, "s", function() hs.application.launchOrFocus("Microsoft Edge") end)
+hs.hotkey.bind(hyper, "z", function() hs.application.launchOrFocus("Google Chrome") end)
+hs.hotkey.bind(hyper, "l", function() hs.application.launchOrFocus("Lark") end)
 
 hs.alert.show("Config loaded")
